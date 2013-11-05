687a52ea8485e889a3cd65258c7ea4570cbc2830
lzoeckle <lzoeckle@macalester.edu>
2013-11-05 11:37:29 -0600

    some weird test thing

--- tests/org/macalester/edu/comp124/lists/TestList.java
+++ tests/org/macalester/edu/comp124/lists/TestList.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 public class TestList {
 	/* Declare a list of Strings here named list */
-	MyLinkedList<String> list = new MyLinkedList<String>();	
+	MyLinkedList<String> list = new MyLinkedList<String>();
 
 	public void prepareList() {
 		list.clear();
